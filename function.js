7303fb74a15608341587f22ba508e25a67db39ed
nandacpc <118414142+nandacpc@users.noreply.github.com>
2024-06-17 03:16:58 -0300

    update

--- function.js
+++ function.js
@@ -19,6 +19,29 @@ function togglePrismaticOverlay() {
 }
 
 function salvarImagem() {
+    var images = document.querySelector(".capture").getElementsByTagName('img');
+    var loaded = 0;
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].complete && images[i].naturalHeight !== 0) {
+            loaded++;
+        } else {
+            images[i].onload = () => {
+                loaded++;
+                if (loaded === images.length) {
+                    captureImage();
+                }
+            };
+            images[i].onerror = () => {
+                alert("Erro ao carregar uma das imagens.");
+            };
+        }
+    }
+    if (loaded === images.length) {
+        captureImage();
+    }
+}
+
+function captureImage() {
     html2canvas(document.querySelector(".capture"), {
         scale: 1,
         width: 224,
@@ -35,7 +58,3 @@ function salvarImagem() {
         alert('Erro ao salvar a imagem. Verifique o console para mais detalhes.');
     });
 }
-
-
-
-
